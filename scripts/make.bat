eda09b8202a8049b1e0f5df752d56bdbd68d9e5b
JoerivanEngelen <joerivanengelen@hotmail.com>
2022-06-09 16:41:43 +0200

    add batch script to build locally

---
+++ scripts/make.bat
@@ -0,0 +1,13 @@
+setlocal
+
+:: activate conda environment created by environment.yml
+call conda activate nhi_docs
+
+:: convert csv master table to .rst files
+call python %~dp0/convert_master_table.py
+
+:: Build .rst files to a html pages
+cd ..
+call sphinx-build -d "docs/_build/docs_doctree" docs "docs/_build/docs_out" --color -bhtml
+
+pause
